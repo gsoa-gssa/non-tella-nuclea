7221636ce0a89fb9fdf117d2d154c47c6cedafec
Timothy Justin Oesch <54284649+timothyoesch@users.noreply.github.com>
2024-03-15 19:42:41 +0100

    Change name str replacement in verify email

--- resources/views/emails/verify-email.blade.php
+++ resources/views/emails/verify-email.blade.php
@@ -161,7 +161,7 @@ table, td { color: #000000; } #u_body a { color: #0000ee; text-decoration: under
       <td style="overflow-wrap:break-word;word-break:break-word;padding:10px;font-family:arial,helvetica,sans-serif;" align="left">
 
   <div style="font-size: 24px; font-weight: 700; color: #000000; line-height: 140%; text-align: left; word-wrap: break-word;">
-    <p style="line-height: 140%;">{{__("Hallo :name", $supporter->data["firstname"])}}</p>
+    <p style="line-height: 140%;">{{__("Hallo :name", ["name" => $supporter->data["firstname"]])}}</p>
   </div>
 
       </td>
