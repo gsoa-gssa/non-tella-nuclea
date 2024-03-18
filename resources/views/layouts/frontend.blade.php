7ba637dd9381b28e34bc4a1e488f911ac8180513
Timothy Justin Oesch <54284649+timothyoesch@users.noreply.github.com>
2024-03-18 16:51:48 +0100

    Add Stuff

--- resources/views/layouts/frontend.blade.php
+++ resources/views/layouts/frontend.blade.php
@@ -39,6 +39,22 @@
     <meta name="theme-color" content="#ffffff">
 
     @vite("resources/css/app.scss")
+    <!--
+    /**
+    * @license
+    * MyFonts Webfont Build ID 793130
+    *
+    * The fonts listed in this notice are subject to the End User License
+    * Agreement(s) entered into by the website owner. All other parties are
+    * explicitly restricted from using the Licensed Webfonts(s).
+    *
+    * You may obtain a valid license from one of MyFonts official sites.
+    * http://www.fonts.com
+    * http://www.myfonts.com
+    * http://www.linotype.com
+    *
+    */
+    -->
 </head>
 <body>
     <main>
