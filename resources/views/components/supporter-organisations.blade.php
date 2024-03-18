7f7894f1de57b156bf570370d059609dc05944d8
Timothy Justin Oesch <54284649+timothyoesch@users.noreply.github.com>
2024-03-18 17:28:31 +0100

    Add GP Logo

--- resources/views/components/supporter-organisations.blade.php
+++ resources/views/components/supporter-organisations.blade.php
@@ -1,4 +1,4 @@
-<div class="petition-supporter-logos grid grid-cols-3 mt-4 gap-4">
+<div class="petition-supporter-logos grid grid-cols-2 md:grid-cols-3 mt-4 gap-4">
     @foreach ($logos as $logo)
         <div class="petition-supporter-logos__item aspect-square flex p-2 bg-white">
             <img src="{{ Storage::url($logo) }}" alt="Logo" class="object-contain" />
