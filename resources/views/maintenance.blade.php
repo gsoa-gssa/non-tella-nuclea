221277907ac4a7fae6ae67f81406beedba59d025
Timothy Justin Oesch <54284649+timothyoesch@users.noreply.github.com>
2024-03-28 09:56:54 +0100

    Remove quickfix and maintenance mode

--- resources/views/maintenance.blade.php
+++
@@ -1,5 +0,0 @@
-<x-frontend>
-    <div class="h-screen flex justify-center items-center">
-        <p class="text-accent text-2xl">This page is under brief maintenance. Please excuse the inconvenience.</p>
-    </div>
-</x-frontend>
