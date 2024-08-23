d372f368cda87b4dc2fa345211a324eb57e81364
Timothy Justin Oesch <54284649+timothyoesch@users.noreply.github.com>
2024-08-23 15:25:37 +0200

    Add different padding at header

--- resources/views/landing/fundraising.blade.php
+++ resources/views/landing/fundraising.blade.php
@@ -1,7 +1,7 @@
 <x-frontend>
 
     <div class="tpnw-petition-header bg-accent">
-            <div class="tpnw-petition-header__inner px-4 pt-4 md:pt-8 lg:pt-12 pb-18 md:pb-24 lg:pb-28  w-fit mx-auto max-w-7xl sm:px-6 lg:px-8 py-6">
+            <div class="tpnw-petition-header__inner px-4 pt-4 md:pt-8 lg:pt-12 pb-20 md:pb-24 lg:pb-28  w-fit mx-auto max-w-7xl sm:px-6 lg:px-8">
                 <x-petition-icon class="max-w-[369px] w-full mb-2 mx-auto" />
                 <h2 class="text-center text-xl md:text-2xl lg:text-3xl">{{__("visual.subtitle")}}</h2>
                 <h1 class="tpnw-title text-center text-3xl md:text-4xl lg:text-5xl">{{__("Atomwaffenverbot jetzt!")}}</h1>
@@ -9,7 +9,7 @@
         </div>
     </div>
 
-    <div class="tpnw-petition-cta -mt-14 md:-mt-16 lg:-mt-24">
+    <div class="tpnw-petition-cta -mt-14 md:-mt-16 lg:-mt-20">
         <div class="tpnw-petition-cta__container px-2 md:px-4">
             <div class="tpnw-petition__container__inner max-w-[690px] mx-auto bg-white p-4 md:p-8 lg:p-10 relative">
                 <p class="text-3xl font-bold mb-4">{{__("fundraising.thanks")}}</p>
